a6be3c28eb6b57148e827908f5ceeb1d3a277eac
urreita9 <francisco.urrea9@gmail.com>
2022-04-10 11:52:36 -0300

    Auth/register - Auth/login

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -2,7 +2,7 @@ const { validationResult } = require('express-validator');
 const bcryptjs = require('bcryptjs');
 const { User } = require('../db/db');
 
-const loginPost = async (req, res) => {
+const postRegister = async (req, res) => {
 	// Check errors in request body
 	const errors = validationResult(req);
 
@@ -29,6 +29,34 @@ const loginPost = async (req, res) => {
 	res.status(201).json(user);
 };
 
+const postLogin = async (req, res) => {
+	// Check errors in request body
+	const errors = validationResult(req);
+
+	if (!errors.isEmpty()) {
+		return res.status(400).json(errors);
+	}
+
+	const { email, password } = req.body;
+
+	// Verify if email exists on db
+	const user = await User.findOne({ where: { email } });
+
+	if (!user) {
+		return res.status(400).json({ msg: 'Wrong Email or Password' });
+	}
+
+	// Compare password with hashed password in db
+	const comparePassword = bcryptjs.compareSync(password, user.password);
+
+	if (!comparePassword) {
+		return res.status(400).json({ msg: 'Wrong Email or Password' });
+	}
+
+	res.status(200).json(user);
+};
+
 module.exports = {
-	loginPost,
+	postRegister,
+	postLogin,
 };
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,20 @@
 require('dotenv').config();
 const { check } = require('express-validator');
 const { Router } = require('express');
-const { loginPost } = require('../controllers/auth');
+const { postRegister, postLogin } = require('../controllers/auth');
 
 const router = Router();
+
+router.post(
+	'/auth/register',
+	[
+		check('email', 'Must be an email').isEmail(),
+		check('password', 'Password must be at least 6 characters long').isLength({
+			min: 6,
+		}),
+	],
+	postRegister
+);
 
 router.post(
 	'/auth/login',
@@ -13,7 +24,7 @@ router.post(
 			min: 6,
 		}),
 	],
-	loginPost
+	postLogin
 );
 
 module.exports = router;
