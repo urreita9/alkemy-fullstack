d94fee9f8d2dd17a83647db508a47d0f716df6ba
urreita9 <francisco.urrea9@gmail.com>
2022-04-21 01:02:59 -0300

    redux. Init store, reducer

---
+++ client/src/redux/reducer/reducer.js
@@ -0,0 +1,16 @@
+const initialState = {
+	operations: [],
+	filteredOperations: [],
+	operation,
+};
+
+const reducer = (state = initialState, action) => {
+	const { type, payload } = action;
+
+	switch (type) {
+		default:
+			return state;
+	}
+};
+
+export default reducer;
