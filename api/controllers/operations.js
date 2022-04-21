417fca5826bd3244683ad100322ec30b1f681667
urreita9 <francisco.urrea9@gmail.com>
2022-04-21 00:27:05 -0300

    post operation route

--- api/controllers/operations.js
+++ api/controllers/operations.js
@@ -1,4 +1,4 @@
-const { User, Operations } = require('../db/db');
+const { User, Operation } = require('../db/db');
 
 const getOperations = async (req, res) => {
 	const { id } = req.params;
@@ -8,20 +8,43 @@ const getOperations = async (req, res) => {
 
 		if (!user) return res.status(404).json({ msg: 'User does not exist' });
 
-		const operations = await Operations.findAll({
+		const operations = await Operation.findAll({
 			where: {
 				UserId: id,
 			},
 		});
 		return res.json({ operations });
 	} catch (error) {
-		return res.status(500).json({ msg: 'Sorry, something went wrong.' });
+		return res.status(500).json({ msg: 'Sorry, something went wrong.', error });
 	}
 };
 
 const postOperation = async (req, res) => {
+	const { id, description, amount, opType } = req.body;
+
+	if (opType !== 'income' && opType !== 'outcome')
+		return res.status(401).json({
+			msg: 'Wrong request. Operation Type must be "income" or "outcome"',
+		});
+
 	try {
-	} catch (error) {}
+		const user = await User.findByPk(id);
+		if (!user) return res.status(404).json({ msg: 'User does not exist' });
+
+		console.log(user);
+		const operation = await Operation.create({
+			description,
+			amount,
+			opType,
+			date: new Date(),
+			UserId: user.id,
+		});
+
+		return res.status(201).json(operation);
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json({ msg: 'Sorry, something went wrong.', error });
+	}
 };
 
 module.exports = {
