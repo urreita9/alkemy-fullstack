5a8bbb48e750889ad89dff27b8a0dd910dff1050
urreita9 <francisco.urrea9@gmail.com>
2022-04-25 23:30:54 -0300

    make it better

--- api/index.js
+++ api/index.js
@@ -6,6 +6,16 @@ const { db } = require('./db/db');
 const server = express();
 server.use(cors());
 server.use(express.json());
+server.use((req, res, next) => {
+	// res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+	res.header('Access-Control-Allow-Credentials', 'true');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept'
+	);
+	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+	next();
+});
 server.use('/api', routes);
 
 const PORT = process.env.PORT || 3000;
